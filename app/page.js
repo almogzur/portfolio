9af437481e6f25f13906529bb82ccfcd41b2a064
almogzur <almogzur1@gmail.com>
2022-12-24 12:30:48 +0200

    section

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 //client componenet 
 'use client'
+import Image from 'next/image'
 
 import  {motion} from 'framer-motion'
 
 export default function Page() {
 
-
     return (
-   <motion.div >
 
+   <motion.div  >
+     <section>
+
+       <Image  id="section-cover" src="/section.jpg" alt="" width={200} height="100" />
+
+    </section>
    </motion.div>
     )
 
