5699d3034cf00e68caa1ce2aff05df9a7aa4130b
almogzur <almogzur1@gmail.com>
2023-02-04 14:10:08 +0200

    update soitecover

--- Components/HomeSection.js
+++ Components/HomeSection.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 import Image from "next/image"
-import sectionBG from '../public/section.png'
+import sectionBG from '../public/sitecover.jpg'
 
 export default function HomeSection (){
 
