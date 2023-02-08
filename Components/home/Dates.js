791c5f8efdbae8b8d227d9bf39a30b5e738447ce
almogzur <almogzur1@gmail.com>
2023-02-08 14:40:53 +0200

    updat cover

--- Components/home/Dates.js
+++ Components/home/Dates.js
@@ -1,17 +1,27 @@
 export default function Dates (){
-    return(<div className="text-center">
-   
-           <label for="start">Start date</label>
-           <input type="date" id="start" name="trip-start" readOnly
+    return(<div className=" dates text-center">
+   <label htmlFor="dropdown">From</label>
+   <select id="dropdown">
+  <option value="one">one</option>
+  <option value="two">two</option>
+  </select>
+  <label htmlFor="dropdown">To</label>
+   <select id="dropdown">
+  <option value="one">one</option>
+  <option value="two">two</option>
+  </select>
+           <label htmlFor="depart">Depart</label>
+           <input type="date" id="start" name="depart" readOnly
                     value="2018-07-22"
                     min="2023-01-01" max="2024-12-31"/> <rb/>
-           <label for="start">End date</label>
-           <input type="date" id="start" name="trip-start" readOnly
+           <label htmlFor="return">Return</label>
+           <input type="date" id="start" name="return" readOnly
                    value="2018-07-22"
                     min="2018-01-01" max="2018-12-31" /> 
                     <br/>
                     
    <button id="serch" className="text-center">Serch</button>
+   
         </div> 
 )
 }
--- Components/home/WelcomeText.js
+++ Components/home/WelcomeText.js
@@ -3,7 +3,7 @@ import CircumIcon from "@klarr-agency/circum-icons-react"
 
 export default function Welcome (){
   return(
-    <div className="welcome text-center" >
+    <div  className="welcome text-center" >
    <p>Business professionals work hard on the daily. So, vacationing calls for playing hard! A nine to five life offers limited excitement. Choose aquatic sports on your vacation that make up for it! 5 Aquatic Sports to Try on Vacation Whether your trip is primarily for business or pleasure,.
    </p>
    </div>
