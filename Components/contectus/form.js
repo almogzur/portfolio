c3e188bd8aff41f0b823aa7a682c5bde2953ac2a
almogzur <almogzur1@gmail.com>
2023-02-22 14:48:33 +0200

    contect us

---
+++ Components/contectus/form.js
@@ -0,0 +1,13 @@
+export default function Form (){
+    return(<div>
+    googelmap
+        <form>
+            name
+            last name
+            eaail
+            phone
+            
+        </form>
+    </div>)
+
+}
